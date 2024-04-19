55331b227e1e77d2b175780649f0b3bb30cbcaa2
Mathis Laroche <ecomath360@gmail.com>
2024-04-19 14:02:42 -0400

    Working Flake
    Working flake

---
+++ README.md
@@ -0,0 +1,3 @@
+# Flake for the [pyexpander](https://pypi.org/project/pyexpander/).
+
+Author of the lib: Goetz Pfeiffer
--- flake.nix
+++ flake.nix
@@ -1,15 +1,50 @@
 {
-  description = "Flake for the pyexpander general macro system";
+  description = "Python library for macro expansion";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        python = pkgs.python3;
+        python_packages = python.withPackages (ps: with ps; [
+          setuptools
+          wheel
+        ]);
+      in
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      {
+        packages.default = with import nixpkgs { inherit system; };
+          python3Packages.buildPythonApplication rec {
+            pname = "pyexpander";
+            version = "2.1.2";
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+            src = fetchPypi {
+              inherit pname version;
+              hash = "sha256:a1ad4804851bf99fcfd467d8ad46308d6aa05cc36f6f443232cd059dada05e84";
+            };
 
-  };
+            pyproject = true;
+
+            nativeBuildInputs = [
+              python_packages
+            ];
+
+            # buildPhase = "tar -xzf pyexpander-2.1.2.tar.gz";
+            # installPhase = "mkdir -p $out/bin; python3 -m pip install ./pyexpander-2.1.2";
+
+            meta = {
+              description = "Python library for macro expansion";
+              homepage = "https://pyexpander.sourceforge.io/introduction.html";
+              license = lib.licenses.gpl3;
+              maintainers = [ "Goetz Pfeiffer" ];
+            };
+          };
+
+      }
+    );
 }
